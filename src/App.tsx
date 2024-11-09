25bccd46cd018ca1b831d4b942425e0ba312c5ec
Maxim Gannota <73496978+maximnota@users.noreply.github.com>
2024-11-09 16:33:57 +0200

    Add code

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "@radix-ui/themes/styles.css";
+import DecryptionScreen from "./components/decryptionScreen";
+
+function App() {
+  return (
+      <DecryptionScreen />
+  );
+}
+
+export default App;
---
+++ src/components/decryptionScreen.tsx
@@ -0,0 +1,8 @@
+import { Text } from "@radix-ui/themes/src/components/text.tsx";
+export default function DecryptionScreen() {
+    return (
+        <>
+            <Text size={"9"} weight="bold">Hi</Text>
+        </>
+    );
+}

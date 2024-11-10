53511af44001054b04ed206d9002d31df47c0061
Maxim Gannota <73496978+maximnota@users.noreply.github.com>
2024-11-10 08:43:29 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "@radix-ui/themes/styles.css";
+import { Theme } from '@radix-ui/themes';
 import DecryptionScreen from "./components/decryptionScreen";
 
 function App() {
   return (
+      <Theme
+      accentColor="crimson"
+      grayColor="sand"
+      radius="large"
+      scaling="95%"
+    >
       <DecryptionScreen />
+    </Theme>
   );
 }
 
